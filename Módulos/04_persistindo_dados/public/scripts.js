6a6ba8625ac2bab641bc1c4ccb8c6a6acf86fcc6
Arnxld <pedro.arnold13@gmail.com>
2021-08-22 19:38:41 -0300

    database module started, classes until the database config 'db.js'

---
+++ Módulos/04_persistindo_dados/public/scripts.js
@@ -0,0 +1,8 @@
+const currentPage = location.pathname // retorna a rota que estou "/members"
+const menuItems = document.querySelectorAll("header .links a")
+
+for (item of menuItems) {
+    if(currentPage.includes(item.getAttribute("href"))) {
+        item.classList.add("active")
+    }
+}
