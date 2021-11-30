b06f2e9496c430eabf86344f0a40aee601a2164e
Arnxld <pedro.arnold13@gmail.com>
2021-11-30 17:48:41 -0300

    knowing promisses

---
+++ Módulos/05_launchstore/src/app/models/Category.js
@@ -0,0 +1,7 @@
+const db = require('../../config/db')
+
+module.exports = {
+    all() {
+        db.query(`SELECT * FROM categories`)
+    }
+}
--- Módulos/05_launchstore/src/routes.js
+++ Módulos/05_launchstore/src/routes.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const routes = express.Router()
-const ProductControler = require("./app/controllers/ProductController")
+const ProductController = require("./app/controllers/ProductController")
 
 routes.get("/", function(req, res) {
     return res.render("layout.njk")
 })
 
-routes.get("/products/create", ProductControler.create)
+routes.get("/products/create", ProductController.create)
 
+routes.post("/products", ProductController.post)
+
+//alias
 routes.get("/ads/create", function(req, res) {
     return res.redirect("/products/create")
 })
