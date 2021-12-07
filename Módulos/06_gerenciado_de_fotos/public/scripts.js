6dc8868b8187e98f28be3b640d5b18c10be9e3a9
Arnxld <pedro.arnold13@gmail.com>
2021-12-07 18:08:20 -0300

    photo manager front end finalized

--- Módulos/06_gerenciado_de_fotos/public/scripts.js
+++ Módulos/06_gerenciado_de_fotos/public/scripts.js
@@ -15,37 +15,71 @@ const Mask = {
 }
 
 const PhotosUpload = {
+    preview: document.querySelector('#photos-preview'),
     uploadLimit: 6,
     handleFileInput(event) {
         const { files: fileList } = event.target
-        const { uploadLimit } = PhotosUpload
+        
 
-        if (fileList.length > uploadLimit) {
-            alert(`Envie no máximo ${uploadLimit} fotos`)
-            event.preventDefault()
-            return
-        }
-
+        if(PhotosUpload.hasLimit(event)) return
 
         // ler as imagens enviadas pelo usuário no front-end
-        Array.from(fileList).forEach((file) => {
+        Array.from(fileList).forEach(file => {
             const reader = new FileReader()
 
             reader.onload = () => {
                 const image = new Image() // <img />
                 image.src = String(reader.result)
 
-                const div = document.createElement('div')
-                div.classList.add('photo')
+                const div = PhotosUpload.getContainer(image)
 
-                div.onclick = () => alert(`remover foto`)
-
-                div.appendChild(image)
-
-                document.querySelector('#photos-preview').appendChild(div)
+                
+                PhotosUpload.preview.appendChild(div)
             }
 
             reader.readAsDataURL(file)
         })
+    },
+    hasLimit(event) {
+        const { uploadLimit } = PhotosUpload
+        const { files: fileList } = event.target
+
+
+        if (fileList.length > uploadLimit) {
+            alert(`Envie no máximo ${uploadLimit} fotos`)
+            event.preventDefault()
+            return true
+        }
+
+        return false
+    },
+    getContainer(image) {
+        const div = document.createElement('div')
+        div.classList.add('photo')
+
+        div.onclick = PhotosUpload.removePhoto
+
+        console.log(typeof(div))
+
+        div.appendChild(image)
+
+        div.appendChild(PhotosUpload.getRemoveButton())
+
+        return div
+    },
+    getRemoveButton() {
+
+        const button = document.createElement('i')
+        button.classList.add('material-icons')
+        button.innerHTML = "close"
+
+        return button
+    },
+    removePhoto(event) {
+        const photoDiv = event.target.parentNode
+        const photosArray = Array.from(PhotosUpload.preview.children)
+        const index = photosArray.indexOf(photoDiv)
+
+        photoDiv.remove()
     }
 }
