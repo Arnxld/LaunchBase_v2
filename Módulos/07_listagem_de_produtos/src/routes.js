605433775ecda881403203ea1576b1e20790ce37
Arnxld <pedro.arnold13@gmail.com>
2021-12-15 12:22:18 -0300

    initialized search page, nunjucks macros for code reuse and fixed header method

--- Módulos/07_listagem_de_produtos/src/routes.js
+++ Módulos/07_listagem_de_produtos/src/routes.js
@@ -3,9 +3,15 @@ const routes = express.Router()
 const multer = require('./app/middlewares/multer')
 const ProductController = require("./app/controllers/ProductController")
 const HomeController = require('./app/controllers/HomeController')
+const SearchController = require('./app/controllers/SearchController')
 
 routes.get("/", HomeController.index)
 
+
+//search
+routes.get('/products/search', SearchController.index)
+
+// products
 routes.get("/products/create", ProductController.create)
 routes.get("/products/:id", ProductController.show)
 routes.get("/products/:id/edit", ProductController.edit)
@@ -20,5 +26,7 @@ routes.delete("/products", ProductController.delete)
 routes.get("/ads/create", function (req, res) {
     return res.redirect("/products/create")
 })
+
+
 
 module.exports = routes
