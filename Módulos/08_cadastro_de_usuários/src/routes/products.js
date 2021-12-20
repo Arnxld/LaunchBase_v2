ca44b500ec3df6aae8d6c6a377db7ed907120d22
Arnxld <pedro.arnold13@gmail.com>
2021-12-20 16:09:44 -0300

    users routes

---
+++ Módulos/08_cadastro_de_usuários/src/routes/products.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const routes = express.Router()
+const multer = require('../app/middlewares/multer')
+
+const ProductController = require("../app/controllers/ProductController")
+const SearchController = require('../app/controllers/SearchController')
+
+
+//search
+routes.get('/search', SearchController.index)
+
+// products
+routes.get("/create", ProductController.create)
+routes.get("/:id", ProductController.show)
+routes.get("/:id/edit", ProductController.edit)
+
+
+// multer para receber arquivos do form
+routes.post("/", multer.array("photos", 6), ProductController.post)
+routes.put("/", multer.array("photos", 6), ProductController.put)
+routes.delete("/", ProductController.delete)
+
+module.exports = routes
