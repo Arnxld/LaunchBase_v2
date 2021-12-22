8b35f15fcb12a2814324d9d34b54779d427b7ea4
Arnxld <pedro.arnold13@gmail.com>
2021-12-22 00:04:56 -0300

    validating if user already exists on registering

--- Módulos/08_cadastro_de_usuários/src/app/controllers/UserController.js
+++ Módulos/08_cadastro_de_usuários/src/app/controllers/UserController.js
@@ -1,6 +1,31 @@
+const User = require('../models/User')
 
 module.exports = {
     registerForm(req, res) {
         return res.render("user/register")
+    },
+    async post(req, res) {
+        const keys = Object.keys(req.body)
+
+        for (key of keys) {
+            if (req.body[key] == "") {
+                return res.send("Please fill all fields")
+            }
+        }
+
+        // check if users already exists [email, cpf_cnpj]
+        let {email, cpf_cnpj, password, passwordRepeat} = req.body
+
+        cpf_cnpj.replace(/D/g, "")
+
+        const user = await User.findOne({
+            where: {email},
+            or: {cpf_cnpj}
+        })
+
+        if (user) return res.send('User exists')
+
+        // check if passwords match
+        if(password != passwordRepeat) return res.send("password mismatch")
     }
 }
