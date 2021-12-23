ccec5ceb2911301d8cfc35f7875aca9672e68c3e
Arnxld <pedro.arnold13@gmail.com>
2021-12-22 22:17:54 -0300

    password hash

--- Módulos/08_cadastro_de_usuários/src/app/controllers/UserController.js
+++ Módulos/08_cadastro_de_usuários/src/app/controllers/UserController.js
@@ -5,6 +5,11 @@ module.exports = {
         return res.render("user/register")
     },
     async post(req, res) {
-        return res.send('passed')
+        const userId = await User.create(req.body)
+
+        return res.redirect('/users')
+    },
+    show(req, res) {
+        return res.send('cadastrado')
     }
 }
