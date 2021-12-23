0685ecb271d07dafb53b84b55f987adbb5c71527
Arnxld <pedro.arnold13@gmail.com>
2021-12-23 00:00:14 -0300

    creating user session

--- Módulos/08_cadastro_de_usuários/src/app/controllers/UserController.js
+++ Módulos/08_cadastro_de_usuários/src/app/controllers/UserController.js
@@ -7,6 +7,9 @@ module.exports = {
     async post(req, res) {
         const userId = await User.create(req.body)
 
+        req.session.userId = userId // adicionando userId ao req.session
+        // ao registrar, a sessão está sendo automaticamente criada
+
         return res.redirect('/users')
     },
     show(req, res) {
--- Módulos/08_cadastro_de_usuários/src/server.js
+++ Módulos/08_cadastro_de_usuários/src/server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
-const methodOverride = require('method-override')
-
 const routes = require('./routes')
 
+const methodOverride = require('method-override')
+const session = require('./config/session')
 const server = express()
 
+server.use(session) // agora req.session está disponível na aplicação
 server.use(express.urlencoded({ extended: true }))
 server.use(express.static('public'))
 server.use(methodOverride('_method'))
